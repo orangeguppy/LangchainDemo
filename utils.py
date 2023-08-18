cd3647dea809d821377449c950938cb00049e979
Nicole Oo <yxyohy@gmail.com>
2023-08-18 14:11:20 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,34 @@
+import pinecone
+from langchain.vectorstores import Pinecone
+from sentence_transformers import SentenceTransformer
+import openai
+import streamlit as st
+openai.api_key = "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"
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+pinecone.init(api_key="9dc27ca5-3576-4f8b-b0b8-4a984567be35", environment="asia-southeast1-gcp-free")
+index = pinecone.Index("langchain-bot")
+
+def query_refiner(conversation, query):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+
+def find_match(input):
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=2, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
